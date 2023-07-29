b67e001371267abd8dd03f5983703a29439922b1
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-07-29 15:01:32 +0200

    v0.2.1: Updated demo app

--- demo.py
+++ demo.py
@@ -4,6 +4,17 @@ import st_login_form
 
 VERSION = st_login_form.__version__
 
+st.set_page_config(
+    page_title="Streamlit Login Form",
+    page_icon="🔐",
+    menu_items={
+        "About": f"Streamlit Login Form 🔐 v{VERSION}  "
+        f"\nApp contact: [Siddhant Sadangi](mailto:siddhant.sadangi@gmail.com)",
+        "Report a Bug": "https://github.com/SiddhantSadangi/st-login-form/issues/new",
+        "Get help": None,
+    },
+)
+
 # ---------- SIDEBAR ----------
 with open("assets/sidebar.html", "r", encoding="UTF-8") as sidebar_file:
     sidebar_html = sidebar_file.read().replace("{VERSION}", VERSION)
@@ -12,7 +23,7 @@ with st.sidebar:
     st.components.v1.html(sidebar_html, height=600)
 
 # ---------- MAIN PAGE ----------
-st.title("[`st-login-form`](https://github.com/SiddhantSadangi/st_login_form) demo app :star: ")
+st.title("🔐 [`st-login-form`](https://github.com/SiddhantSadangi/st_login_form) demo app ")
 
 st.write(
     "This app shows how you can use the `st-login-form` component to create user-login forms for Streamlit apps."
